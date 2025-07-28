595823bffc022f584568bc5de065531d83f0b703
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-28 20:31:36 +0200

    correct the expected output from Generator to List

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,17 +2,16 @@
 """
 A module that defines an asynchronous omprehension
 """
-import random
-import asyncio
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Coroutine that collects 10 random numbers and returns them
 
     Returns:
     10 random numbers
     """
-    return [num async for num in async_generator()]
+    numbers = [num async for num in async_generator()]
+    return numbers
