90dd29e3f2308d6c29fab7abf0743964d5cbf0f3
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-28 20:26:43 +0200

    add 1-async_comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A module that defines an asynchronous omprehension
+"""
+import random
+import asyncio
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """
+    Coroutine that collects 10 random numbers and returns them
+
+    Returns:
+    10 random numbers
+    """
+    numbers = [num async for num in async_generator()]
+    return numbers
