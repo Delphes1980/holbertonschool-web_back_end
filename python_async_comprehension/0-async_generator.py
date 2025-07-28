7f3b154802982be4953fdb39e405ac588226d07f
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-28 20:20:17 +0200

    import Generator and not AsyncGenerator in 0-async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ A module that defines an asynchronous generator
 """
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that loops 10 times, wait 1 second each time,
     and yields a random number between 0 and 10
