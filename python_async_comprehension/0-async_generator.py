3e101de9b78b1d8313fbb8adac66fbe665166571
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-28 20:14:43 +0200

    change _ for i in the for loop

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -12,7 +12,7 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Coroutine that loops 10 times, wait 1 second each time,
     and yields a random number betwee 0 and 10
     """
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
     return
