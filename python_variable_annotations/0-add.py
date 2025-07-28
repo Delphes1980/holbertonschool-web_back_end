7f967b233fe7eb523178ed90d6bc371173795446
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-28 10:07:07 +0200

    add spaces instead of tabulations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,14 +4,14 @@ This module defines a type function with expected type of arguments and result
 """
 
 def add(a: float, b:float) -> float:
-	"""
+    """
     A function that defines the type of arguments expected
 
     Args:
     a (float): first argument expected as a float
     b (float): second argument expected as a float
 
-	Return:
-	A float is expected as a result
+    Return:
+    A float is expected as a result
     """
-	return a + b
+    return a + b
