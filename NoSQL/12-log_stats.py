3757f95618cf95664322202423d3eec9079b0672
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-08-12 19:02:50 +0200

    add some comments and add a dictionary to put the result of the aggregation into

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,11 +7,8 @@ from pymongo import MongoClient
 
 def log_stat():
     """
-    The function that inserts a new document in a collection
-
-    Args:
-        mongo_collection: the collection object
-        topics: the specific topic searched
+    Connects to the MongoDB 'logs' database and 'nginx' collection
+    and print some statistics
     """
     # Connexion to the server
     client = MongoClient("mongodb://localhost:27017")
@@ -23,18 +20,24 @@ def log_stat():
 
     print("Methods:")
 
-    # Return the total of documents sorted by method using aggregation
+    # Return the total of documents grouped by method using aggregation
     method_number = logs_collection.aggregate([
         {"$group": {"_id": "$method", "count": {"$sum": 1}}}
     ])
 
+    # Store the aggregation result in a dictionary
+    method_counts = {item['_id']: item['count'] for item in method_number}
+
     # Get the number of documents by method
     methods_to_check = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    # Iterate through the list and print the corresponding count
+    # from the dictionary
     for method in methods_to_check:
         count = method_number.get(method, 0)
         print(f"\t method {method}: {count}")
 
-    # Get the number of documents with the method GET
+    # Get the number of documents with the method GET and path /status
     status_log = logs_collection.count_documents({"method": "GET",
                                                   "path": "/status"})
     print(f"{status_log} status check")
