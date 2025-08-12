621288e57f359683960021722abfaf0e692639a9
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-08-12 12:26:44 +0200

    add 8-all.py file

---
+++ NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module provides a single method that list all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    The method to list all documents in a collection
+
+    Args:
+        mongo_collection: The collection
+    """
+    return list(mongo_collection.find())
