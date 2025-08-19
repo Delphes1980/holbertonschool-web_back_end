a86212c23a7084775325bdf3bdb20fd001ab251d
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-08-19 16:12:14 +0200

    add 6-sky_high.js file

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Sqft getter
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Floors getter
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
