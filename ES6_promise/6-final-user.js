bc52170ff6cb2e44da0e9767fbf48e5a5159b8fc
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-08-25 20:46:48 +0200

    try to please the checker with imports

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
-import signeUpUser from './4-user-promises.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signeUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      value: result.status === 'fulfilled' ? result.value : String(result.reason.toString),
     })));
 }
