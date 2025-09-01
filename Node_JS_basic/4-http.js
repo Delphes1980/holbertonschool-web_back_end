cd13702f1d92d48b9602c763f983ace8b0faa420
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-09-01 19:54:52 +0200

    try to please the checker

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,6 +3,8 @@ const http = require('node:http');
 const app = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello Holberton School!');
-}).listen(1245);
+});
+
+app.listen(1245);;
 
 module.exports = app;
