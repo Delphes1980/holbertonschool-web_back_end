fd25e1c3703d034da5ad6a3c15e2e386c0bdc574
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-09-01 19:53:13 +0200

    add 4-http.js file

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const http = require('node:http');
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello Holberton School!');
+}).listen(1245);
+
+module.exports = app;
