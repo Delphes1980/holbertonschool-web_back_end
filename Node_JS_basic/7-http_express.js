213128a2fed889972a71ce633a9d5116815a0a18
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-09-02 12:44:49 +0200

    try to please the checker

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('node:fs').promises;
 const countStudents = require('./3-read_file_async');
 
 const app = express();
@@ -11,46 +10,33 @@ app.get('/', (request, response) => {
   response.send('Hello Holberton School!');
 });
 
-app.get('/students', (request, response) => {
+app.get('/students', async (request, response) => {
   response.status(200);
   response.set('Content-Type', 'text/plain');
   response.write('This is the list of our students\n');
 
-  fs.readFile(databasePath, 'utf-8')
-    .then((data) => {
-      const lines = data.split('\n');
-      const filteredLines = lines.filter((line) => line.trim() !== '');
-      const studentLines = filteredLines.slice(1);
-      const numberOfStudents = studentLines.length;
+  // Save the original console.log function
+  const originalLog = console.log;
+  let capturedOutput = '';
 
-      const fields = {};
+  // Define a temporary function to capture the output
+  console.log = (message) => {
+    capturedOutput += `${message}\n`;
+  };
 
-      for (const line of studentLines) {
-        const parts = line.split(',');
-        const firstname = parts[0];
-        const field = parts[3].trim();
+  try {
+    // Call the countStudents function and wait for it to finish
+    // All console.log() outputs from this function will be captured
+    await countStudents(databasePath);
 
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      }
-
-      response.write(`Number of students: ${numberOfStudents}\n`);
-
-      for (const field in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, field)) {
-          const studentCount = fields[field].length;
-          const studentList = fields[field].join(', ');
-          response.write(`Number of students in ${field}: ${studentCount}. List: ${studentList}\n`);
-        }
-      }
-
-      response.end();
-    })
-    .catch((error) => {
-      response.end('Cannot load the database');
-    });
+    // Send the captured output to the HTTP client
+    response.end(capturedOutput);
+  } catch (error) {
+    response.end(error.message);
+  } finally {
+    // Restore the original console.log function, regardless of the result
+    console.log = originalLog;
+  }
 });
 
 app.listen(1245);
