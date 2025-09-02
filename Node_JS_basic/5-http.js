adf4d61a7c25b55431946931273006a1dd6ae383
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-09-02 12:28:58 +0200

    try to please the checker

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,11 +1,11 @@
 const http = require('node:http');
-const fs = require('node:fs').promises;
 const countStudents = require('./3-read_file_async');
 
 const databasePath = process.argv[2];
 
 const app = http.createServer(async (request, response) => {
   response.setHeader('Content-Type', 'text/plain');
+
   if (request.url === '/') {
     response.statusCode = 200;
     response.end('Hello Holberton School!');
@@ -13,40 +13,29 @@ const app = http.createServer(async (request, response) => {
     response.statusCode = 200;
     response.write('This is the list of our students\n');
 
-    fs.readFile(databasePath, 'utf-8')
-      .then((data) => {
-        const lines = data.split('\n');
-        const filteredLines = lines.filter((line) => line.trim() !== '');
-        const studentLines = filteredLines.slice(1);
-        const numberOfStudents = studentLines.length;
+    // Sauvegarder la fonction console.log originale
+    const originalLog = console.log;
+    let capturedOutput = '';
 
-        const fields = {};
+    // Définir une fonction temporaire pour capturer la sortie
+    console.log = (message) => {
+      capturedOutput += `${message}\n`;
+    };
 
-        for (const line of studentLines) {
-          const parts = line.split(',');
-          const firstname = parts[0];
-          const field = parts[3].trim();
+    try {
+      // Appeler la fonction countStudents et attendre qu'elle termine
+      // Toutes les sorties de console.log() de cette fonction seront capturées
+      await countStudents(databasePath);
 
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(firstname);
-        }
-
-        response.write(`Number of students: ${numberOfStudents}\n`);
-
-        for (const field in fields) {
-          if (Object.prototype.hasOwnProperty.call(fields, field)) {
-            const studentCount = fields[field].length;
-            const studentList = fields[field].join(', ');
-            response.write(`Number of students in ${field}: ${studentCount}. List: ${studentList}\n`);
-          }
-        }
-        response.end();
-      })
-      .catch((error) => {
-        response.end('Cannot load the database');
-      });
+      // Renvoyer la sortie capturée au client HTTP
+      response.end(capturedOutput);
+    } catch (error) {
+      // Gérer l'erreur si le fichier n'est pas trouvé
+      response.end(error.message);
+    } finally {
+      // Restaurer la fonction console.log originale, peu importe le résultat
+      console.log = originalLog;
+    }
   } else {
     response.statusCode = 404;
     response.end('Not found');
@@ -54,5 +43,4 @@ const app = http.createServer(async (request, response) => {
 });
 
 app.listen(1245);
-
 module.exports = app;
