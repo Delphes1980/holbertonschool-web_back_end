dacbce36413d394bab8ee7096b184b9f7aa464b8
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-09-02 09:40:56 +0200

    try to have the correct output

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,21 +4,36 @@ const countStudents = require('./3-read_file_async');
 const databasePath = process.argv[2];
 
 const app = http.createServer(async (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.setHeader('Content-Type', 'text/plain');
 
-  if (request.url == '/') {
+  if (request.url === '/') {
+    response.statusCode = 200;
     response.end('Hello Holberton School!');
-  } else if (request.url == '/students') {
+  } else if (request.url === '/students') {
+    response.statusCode = 200;
     response.end('This is the list of our students');
-  
+
+    const originalLog = console.log;
+    let capturedOutput = '';
+
+      console.log = (message) => {
+        capturedOutput += `${message}\n`;
+      };
+
     try {
       await countStudents(databasePath);
-      response.end();
+      console.log = originalLog;
+      response.end(capturedOutput);
     } catch (error) {
+      console.log = originalLog;
       response.end(error.message);
     }
+  } else {
+    response.statusCode = 404;
+    response.end('Not found');
   }
 });
-  app.listen(1245);
+
+app.listen(1245);
 
 module.exports = app;
